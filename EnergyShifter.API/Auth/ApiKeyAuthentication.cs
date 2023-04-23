1a6ca8681251b2585787c2fbefc206a6cea89933
Sam Medawatta <sampath.medawatta@gmail.com>
2023-04-24 00:12:16 +1000

    add API key authentication Dev user: Denis

---
+++ EnergyShifter.API/Auth/ApiKeyAuthentication.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EnergyShifter.API.Auth
+{
+    public class ApiKeyAuthentication : Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyHeaderName = "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = configuration.GetValue<string>(key: "ApiKey");
+
+            if (!apiKey.Equals(potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            await next();
+        }
+    }
+}
---
+++ EnergyShifter.API/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using EnergyShifter.API.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnergyShifter.API.Controllers
+{
+    [ApiKeyAuthentication]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpGet("HealthCheck")]
+        public ActionResult HealthCheck()
+        {
+
+            return Ok();
+
+        }
+    }
+}
