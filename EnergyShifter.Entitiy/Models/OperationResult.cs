a9dbb1b5d7fac7e7359d47a0e4a383eaf07435d2
Sam Medawatta <sampath.medawatta@gmail.com>
2023-04-24 02:03:44 +1000

    create auth response object and operation result object

---
+++ EnergyShifter.Entitiy/Models/OperationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyShifter.Entitiy.Models
+{
+    public class OperationResult
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Error { get; set; }
+        public dynamic Data { get; set; }
+    }
+}
