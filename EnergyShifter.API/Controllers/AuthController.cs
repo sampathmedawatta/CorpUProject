5202053cba3dc7436376e924ea04d731a87703c3
Sam Medawatta <sampath.medawatta@gmail.com>
2023-04-24 02:04:52 +1000

    add JWT authantication Dev User : Denis

--- EnergyShifter.API/Controllers/AuthController.cs
+++ EnergyShifter.API/Controllers/AuthController.cs
@@ -1,6 +1,14 @@
 ﻿using EnergyShifter.API.Auth;
+using EnergyShifter.API.Auth.Interfaces;
+using EnergyShifter.Common;
+using EnergyShifter.Entitiy.Models;
+using EnergyShifter.Entitiy.Models.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Net;
 
 namespace EnergyShifter.API.Controllers
 {
@@ -9,13 +17,57 @@ namespace EnergyShifter.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly ITokenGenerator _tokenGenerator;
+        private readonly JwtTokenSettings _jwtTokenSettings;
+        OperationResult _or;
+        public AuthController( ITokenGenerator tokenGenerator,  IOptions<JwtTokenSettings> jwtTokenSettings)
+        {
+            this._tokenGenerator = tokenGenerator;
+            this._jwtTokenSettings = jwtTokenSettings.Value;
+        }
 
+        [AllowAnonymous]
         [HttpGet("HealthCheck")]
         public ActionResult HealthCheck()
         {
-
             return Ok();
+        }
 
+        [HttpGet()]
+        public ActionResult GetToken()
+        {
+            try
+            {
+                AuthanticationResponse authanticationResponse = _tokenGenerator.GenerateToken(_jwtTokenSettings.JWT_Secret, "UserName", double.Parse(_jwtTokenSettings.TokenLifeTime));
+
+                if (authanticationResponse == null)
+                {
+                    _or = new OperationResult
+                    {
+                        Error = "Can not generate the token.",
+                        StatusCode = (int)HttpStatusCode.NotFound,
+                        Data = null
+                    };
+                    return Unauthorized(_or);
+                }
+
+                _or = new OperationResult
+                {
+                    Message = "token generated.",
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Data = authanticationResponse
+                };
+            }
+            catch (Exception ex)
+            {
+                _or = new OperationResult
+                {
+                    Message = "Error: token generation failed.",
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Data = null
+                };
+            }
+            return Ok(_or);
         }
     }
 }
