c667306acef0d67ad803024c63d7af212a6c185c
Sam Medawatta <sampath.medawatta@gmail.com>
2023-05-01 14:39:34 +1000

    Ef core code first steps to create the DB

--- CorpU.Data/DataContext.cs
+++ CorpU.Data/DataContext.cs
@@ -22,6 +22,16 @@ namespace CorpU.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // Use this only when runnung code first migration scripts
+            //"Data Source=LAPTOP-198T1MOJ;Initial Catalog=CorpU_DB_v1;Integrated Security=True;TrustServerCertificate=True; User Id=sa;Password=123456;";
+
+            // select CorpU.Data project as a default project under "package manager console"
+            // Startup project should be Corup.API
+
+            // Add migraton : add-migration InitialMigration
+            // Create uppdate database : Update - Database
+            // https://www.c-sharpcorner.com/UploadFile/26b237/code-first-migrations-in-entity-framework/
+
             optionsBuilder.UseSqlServer(_ConnectionString);
         }
     }
