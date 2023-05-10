cb3044effd270d1b776ea03448b35b5c12abb2c7
Sam Medawatta <sampath.medawatta@gmail.com>
2023-05-10 16:46:17 +1000

    setup and email service interface, class and data models. Installed MimeKit and AspNetCore Hosting bundle to send email.

---
+++ CorpU.Common/Communication/EmailSender.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorpU.Common.Communication
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailSettings _emailSettings;
+        private IHostingEnvironment _env;
+
+        public EmailSender(EmailSettings emailConfig, IHostingEnvironment env)
+        {
+            _emailSettings = emailConfig;
+            _env = env;
+        }
+        public async Task SendEmailAsync(Message message)
+        {
+
+        }
+
+    }
+}
---
+++ CorpU.Common/Communication/Message.cs
@@ -0,0 +1,27 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorpU.Common.Communication
+{
+    public class Message
+    {
+        public List<MailboxAddress> To { get; set; }
+        public string Subject { get; set; }
+        public string TemplateName { get; set; }
+        public dynamic Data { get; set; }
+
+        public Message(string subject, string templateName, dynamic data)
+        {
+            //To = new List<MailboxAddress>();
+
+            //To.AddRange(to.Select(x => new MailboxAddress("",x)));
+            Subject = subject;
+            TemplateName = templateName;
+            Data = data;
+        }
+    }
+}
