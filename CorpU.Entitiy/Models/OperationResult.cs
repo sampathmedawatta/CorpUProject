935b1502572d866e45b8fe0f5d4d675fbca85dcb
Sam Medawatta <sampath.medawatta@gmail.com>
2023-05-15 17:37:18 +1000

    update operation result object and user control parameter type

--- CorpU.Entitiy/Models/OperationResult.cs
+++ CorpU.Entitiy/Models/OperationResult.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CorpU.Entitiy.Models
 {
     public class OperationResult
     {
+        [JsonPropertyName("statusCode")]
         public int StatusCode { get; set; }
+        [JsonPropertyName("message")]
         public string? Message { get; set; }
+        [JsonPropertyName("error")]
         public string? Error { get; set; }
+        [JsonPropertyName("data")]
         public dynamic? Data { get; set; }
     }
 }
