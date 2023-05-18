bb4d771d5190ea5973a17bb28a48b3ba9b4f300d
pratikrakash <pratikrakash@gmail.com>
2023-05-18 12:06:46 +1000

    Added ClassType controller and implemented Get methods.

--- CorpU.Data/Repository/UnitOfWork.cs
+++ CorpU.Data/Repository/UnitOfWork.cs
@@ -25,6 +25,7 @@ namespace CorpU.Data.Repository
                 return new DataContext(ConnectionString);
             }
         }
+        public IClassTypeRepository<ClassTypeDto> ClassType { get;private set; }
         public IUnitRepository<UnitDto> Unit { get; private set; }
         public IApplicantAvailabilityRepository<ApplicantAvailabilityDto> ApplicantAvailability { get; private set; }
         public IVacancyTypeRepository<VacancyTypeDto> VacancyType { get; private set; }
@@ -40,6 +41,7 @@ namespace CorpU.Data.Repository
             ConnectionString = appSetting.Value.DBConnection;
             Qualifications = new QualificationRepository(Context, mapper);
             Unit=new UnitRepository(Context, mapper);
+            ClassType=new ClassTypeRepository(Context, mapper);
             Applicants = new ApplicantRepository(Context, mapper);
             Vacancy=new VacancyRepository(Context, mapper);
             VacancyType=new VacancyTypeRepository(Context, mapper);
