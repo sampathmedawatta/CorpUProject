1285d342d01813b4b6f20d0034a580a01498e1f3
pratikrakash <pratikrakash@gmail.com>
2023-05-19 23:28:39 +1000

    Created new data model for shortlisted applicant and applied mappings.

---
+++ CorpU.Data/Profiles/ShortlistedApplicantProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CorpU.Data.Models;
+using CorpU.Entitiy.Models.Dto.Shortlist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorpU.Data.Profiles
+{
+    public class ShortlistedApplicantProfile : Profile
+    {
+        public ShortlistedApplicantProfile()
+        {
+            //Get
+            CreateMap<ShortlistedApplicantEntity, ShortlistDetailDto>();
+            CreateMap<ShortlistedApplicantEntity, ShortlistDetailDto>()
+            .ForPath(dest =>
+           dest.application.Application_id,
+           opt => opt.MapFrom(src => src.application.Application_id));
+
+            //Set
+            CreateMap<ShortlistDetailDto, ShortlistedApplicantEntity>();
+
+        }
+    }
+}
